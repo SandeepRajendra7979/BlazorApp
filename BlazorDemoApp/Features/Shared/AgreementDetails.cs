9ab41719e7f0047a7d1653d2fb57e9f3f898a91b
v-sarajendra <v-sarajendra@SANDEEPRAJENDRA>
2024-05-07 17:33:21 +0530

    Add project files.

---
+++ BlazorDemoApp/Features/Shared/AgreementDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorDemoApp.Features.Shared
+{
+	public class AgreementDetails
+	{
+		public string SelectedValue { get; set; }
+		public string FullName { get; set; }
+		public string Email { get; set; }
+		public bool SaveForLater { get; set; }
+		public bool AcceptTerms { get; set; }
+	}
+}
---
+++ BlazorDemoApp/Features/Shared/User.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorDemoApp.Features.Shared
+{
+    public class User
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
